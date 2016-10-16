fac905010f22882742f5f95d5d9cceca96c644bb
CCDevelopment <ddclistings@gmail.com>
2016-10-16 09:32:48 +0800

    Overlay remove on click function complete

--- js/lightbox.js
+++ js/lightbox.js
@@ -1,32 +1,35 @@
 
 
-//1. Capture the click event on a link to an image
-
+// Capture the click event on a link to an image
 $('a[href$=".jpg"]').click ( function ( event ) {
-var selectedpicture = $( this ).attr('href');
+
 // get childs alt atrribute and set caption
+var selectedpicture = $( this ).attr('href');
 var caption = $(this).children().attr('alt');
+
+// update the overlay with the image linked in the link
 var html =  '<div id="lightbox"><img src="'+ selectedpicture +'"></div>';
 console.log(selectedpicture);
 console.log(caption);
 event.preventDefault();
 console.log('got it');
-$('#gallery').fadeOut(600);
+// $('#gallery').fadeOut(600);
 $('body').append(html);
+
+// show the overlay
 $('#lightbox').show();
 console.log(html);
+
+//when overlay is clicked, hide the overlay
+$('#lightbox').click( function () {
+  $(this).remove();
+  });
+
+
+
 });
 
-
-
-
-
-  //1.1 show the overlay
-
-  //1.2 update the overlay with the image linked in the link
 
 //2. Add Overlay
   //2.1 an image
   //2.2 a caption
-//3. when overlay is clicked
-  //3.1 hide the overlay
