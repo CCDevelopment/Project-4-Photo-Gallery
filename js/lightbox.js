8c5c2bd084d5a54a62123d7030878847381bb07c
CCDevelopment <ddclistings@gmail.com>
2016-10-16 10:26:28 +0800

    768 media query light box styles
    Appended to HTML
    - captions
    - navigation

--- js/lightbox.js
+++ js/lightbox.js
@@ -6,19 +6,24 @@ $('a[href$=".jpg"]').click ( function ( event ) {
 // get childs alt atrribute and set caption
 var selectedpicture = $( this ).attr('href');
 var caption = $(this).children().attr('alt');
+var navigationright = '<span class="navright">&#8250;</span>';
+var navigationleft = '<span class="navleft">&#8249;</span>';
 
-// update the overlay with the image linked in the link
-var html =  '<div id="lightbox"><img src="'+ selectedpicture +'"></div>';
+// /Creat and Update the overlay with the image linked in the link, check values in console.
+var html =  '<div id="lightbox">'+ navigationleft + '<img src="'+ selectedpicture +'">' + navigationright + '<p>'+ caption + '</p></div>';
 console.log(selectedpicture);
 console.log(caption);
-event.preventDefault();
+console.log(html);
 console.log('got it');
-// $('#gallery').fadeOut(600);
+
+event.preventDefault();
+
+
+//Add the overlay on page
 $('body').append(html);
 
 // show the overlay
 $('#lightbox').show();
-console.log(html);
 
 //when overlay is clicked, hide the overlay
 $('#lightbox').click( function () {
@@ -30,6 +35,5 @@ $('#lightbox').click( function () {
 });
 
 
-//2. Add Overlay
   //2.1 an image
   //2.2 a caption
